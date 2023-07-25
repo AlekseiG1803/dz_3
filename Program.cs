a5669fefb801fc242f208e9fff2c2cc9ef3cd811
Aleksei Golovkov <alekseig1803@gmail.com>
2023-07-25 21:55:57 +0300

    Програма по блок-схеме С#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int[] numbers = new int[] {2, 5, 13, 7, 6, 4};
+double size = numbers.Length;
+double sum = 0;
+double avg = 0;
+int index = 0;
+
+for (index = 0; index < size; index ++) {
+    sum += numbers[index]; 
+}
+
+avg = sum/size;
+
+WriteLine(avg);
